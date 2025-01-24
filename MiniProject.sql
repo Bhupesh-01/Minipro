55d8df0c78e20f64950cfab30cef3bb5f9c485ff
BhupeshPawara <bhupeshpawara01@gmail.com>
2025-01-24 18:40:23 +0530

    configuring

---
+++ MiniProject.sql
@@ -0,0 +1,6 @@
+create database miniproject;
+use miniproject;
+drop database miniproject;
+
+create table Student_details( name varchar(20), Rollno int(20), Class Varchar(20));
+select *from Student_details;
