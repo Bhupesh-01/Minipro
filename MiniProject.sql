f75ed72b1dfcd737096451027996f6214c69cac8
Bhupesh Pawara <134034434+Bhupesh-01@users.noreply.github.com>
2025-02-10 16:21:22 +0530

    queries related
    to see desc

--- MiniProject.sql
+++ MiniProject.sql
@@ -1,6 +1,9 @@
 create database miniproject;
 use miniproject;
-drop database miniproject;
 
-create table Student_details( name varchar(20), Rollno int(20), Class Varchar(20));
+create table Student_details( name varchar(20), 
+                              Rollno int(20), 
+                              Class Varchar(20)
+                             );
+desc Student_details;
 select *from Student_details;
